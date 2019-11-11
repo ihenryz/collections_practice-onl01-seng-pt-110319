0dc2444dc396d313e4263eb14c8a03334915ed4c
Henry Phan <h.phan06@gmail.com>
2019-11-11 21:28:25 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
+require 'pry'
+
+def sort_array_asc(array)
+  array.sort
+end
+
+def sort_array_desc(array)
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort_by{ |array| array.count(array) }
+end
+
+def swap_elements(names)
+  temp = names[1]
+  names[1] = names[names.length - 1]
+  names[names.length - 1] = temp
+  names
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each {|a| new_array << a.sub(a[2], "$")}
+  new_array
+end
+
+def find_a(fruit)
+  fruit.select {|fruit| fruit.start_with?("a")}
+end
+
+def sum_array(nums_array)
+  nums_array.inject {|sum, n| sum + n}
+end
+
+def add_s(array)
+  array.each_with_index.collect { |array, index| array.concat("s") if index != 1}
+end
 
